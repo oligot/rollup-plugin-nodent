effc70a80475161a753635d03a765111d84fccee
Olivier Ligot <olivier.ligot@groups.be>
2017-10-06 13:12:24 +0200

    Improve sourcemap option handling
    The options are now resolved in this order
    * plugin options
    * Rollup options
    * Old style (sourceMap) Rollup options

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const nodent = require('nodent')();
 const createFilter = require('rollup-pluginutils').createFilter;
 
-module.exports = function (options) {
-	options = options || {};
+module.exports = function (options = {}) {
 	const filter = createFilter(options.include, options.exclude);
 
 	return {
 		name: 'nodent',
 		options(opts) {
-			// Convert from Rollup style source map to nodent style sourcemap
-			options.sourcemap = opts.sourceMap || false;
+			options.sourcemap = 'sourcemap' in options ? options.sourcemap // Plugin options
+			: 'sourcemap' in opts ? opts.sourcemap // Rollup options
+			: opts.sourceMap !== false; // Old style Rollup options
 		},
 		transform(code, id) {
 			if (!filter(id)) {
