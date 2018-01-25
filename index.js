18017ade9d0d537be83edcdaeb762bdf304fe1f1
Olivier Ligot <oligot@protonmail.com>
2018-01-25 12:03:44 +0100

    Dynamic import support
    [dynamic import](https://github.com/tc39/proposal-dynamic-import) support has just landed in [Rollup 0.55.0](https://github.com/rollup/rollup/blob/master/CHANGELOG.md#upcoming-0550).
    When testing a project that uses nodent via rollup-plugin-nodent, I noticed that it doesn't support dynamic import yet, resulting in the error
    
    ```
    SyntaxError: Unexpected token in ...
    ```
    
    Looking at the [rollup source code](https://github.com/rollup/rollup/blob/089c9e06190b894d766fc44571752fa91dc1eaa5/src/Graph.ts#L3), I see that they are using [acorn-dynamic-import](https://github.com/kesne/acorn-dynamic-import).
    This patches the acorn version that is used in nodent to add the acorn-dynamic-import plugin as done in rollup itself.
    That way, the nodent compiler is able to parse code that uses dynamic import.
    
    This is quite similar to what has been done for [buble](https://github.com/Rich-Harris/buble/pull/102).
    
    Also switch from xo to eslint and eslint-config/plugin-*

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const nodent = require('nodent')();
+const path = require('path');
+const importFrom = require('import-from');
+// Patch acorn used in nodent-compiler to support dynamic import
+const acorn = importFrom(path.dirname(require.resolve('nodent-compiler')), 'acorn');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('acorn-dynamic-import/lib/inject').default(acorn);
+const NodentCompiler = require('nodent-compiler');
 const createFilter = require('rollup-pluginutils').createFilter;
 
 module.exports = function (options) {
 	options = options || {};
 	const filter = createFilter(options.include, options.exclude);
+	const compiler = new NodentCompiler();
 
 	return {
 		name: 'nodent',
 		options(opts) {
 			options.sourcemap = 'sourcemap' in options ? options.sourcemap : // Plugin options
-			'sourcemap' in opts ? opts.sourcemap : // Rollup options
-			opts.sourceMap !== false; // Old style Rollup options
+				'sourcemap' in opts ? opts.sourcemap : // Rollup options
+					opts.sourceMap !== false; // Old style Rollup options
+			Object.assign(options, {
+				parser: {
+					plugins: {
+						dynamicImport: true
+					}
+				}
+			});
 		},
 		transform(code, id) {
 			if (!filter(id)) {
 				return;
 			}
-			const res = nodent.compile(code, id, null, options);
+			const res = compiler.compile(code, id, options);
 			return {
 				code: res.code,
 				map: options.sourcemap ? res.sourcemap : {mappings: ''}
